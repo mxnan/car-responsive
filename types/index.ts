58c09d1c9390d230e42f38d8e81951443d7f7b2b
mxnan <128995981+mxnan@users.noreply.github.com>
2023-10-02 00:56:02 +0530

    add api next

--- types/index.ts
+++ types/index.ts
@@ -6,6 +6,11 @@ export interface CustomButtonProps {
   handleClick?:
   MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+}
 
-
+export interface SearchManufacturerProps
+{
+  manufacturer: string;
+  setManufacturer: (manufacturer:string) => void;
+  
 }
