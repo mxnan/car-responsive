a61f45cf32eb8a30a29457370dc25458baa3579c
mxnan <128995981+mxnan@users.noreply.github.com>
2023-10-02 17:29:26 +0530

    completing Card for cars

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import { CustomFilter , SearchBar , Hero } from "@/components";
+import { CustomFilter , SearchBar , Hero, CarCard } from "@/components";
+import { fetchCars } from "@/utils";
 
 
-export default function Home() {
+export default async function Home() {
+   const allCars = await fetchCars();
+
+   const isDataEmpty = !Array.isArray(allCars) ||
+                      allCars.length < 1 ||
+                       !allCars;
+   
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -9,6 +17,7 @@ export default function Home() {
          <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
+         </div>
           <div className="home__filters">
             <SearchBar />
             <div className="home__filter-container">
@@ -16,7 +25,27 @@ export default function Home() {
               <CustomFilter title="year" />
             </div>
           </div>
-         </div>
+         
+         {!isDataEmpty 
+          ? 
+            (
+            <section>
+              <div className="home__cars-wrapper">
+                {allCars?.map((car) => (
+                  <CarCard car={car} />
+                ))}
+              </div>
+            </section>
+            ) 
+          :
+            (
+              <div className="home__error-container">
+                <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+                <p>{allCars?.message}</p>
+                </div>
+            )
+         }
+
       </div>
     </main>
   )
--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,4 @@
+import { ChildProcessWithoutNullStreams } from "child_process";
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
@@ -13,4 +14,21 @@ export interface SearchManufacturerProps
   manufacturer: string;
   setManufacturer: (manufacturer:string) => void;
   
+}
+
+export interface CarProps 
+{
+ city_mpg:number;
+ class:string;
+ combination_mpg:number;
+ cylinders:number;
+ displacement:number;
+ drive:string;
+ fuel_type:string;
+ highway_mpg:number;
+ make:string;
+ model:string;
+ transmission:string;
+ year:number;
+ 
 }
