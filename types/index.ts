64565b3cb09921ace0b62a1b0050d56d98e6e51d
mxnan <128995981+mxnan@users.noreply.github.com>
2023-10-07 23:21:15 +0530

    styled modal in cardetails

--- types/index.ts
+++ types/index.ts
@@ -7,6 +7,10 @@ export interface CustomButtonProps {
   handleClick?:
   MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+  textStyles ?: string;
+  rightIcon ?: string;
+  isDisabled ?: boolean;
+  
 }
 
 export interface SearchManufacturerProps
